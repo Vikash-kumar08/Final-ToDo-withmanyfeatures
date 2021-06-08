0c73c68e48a51e0ceaf1655fec5af1c6ee25eec9
Vikash-kumar08 <vikash.kumar@citymall.live>
2021-06-08 13:41:36 +0530

    justcommit

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,61 +1,109 @@
 <template>
-  <div>
-    <nav>  
-    <h> ToDo List</h>
-    </nav>
-    <table border="2px" align="center">
-        <tr>
-            <td>ToDo_name</td>
-            <td>ToDo_time</td>
-            <td>Action</td>
-            <td><button>Delete</button></td>
-        </tr>
-        <tr v-for = "todo in todolist" v-bind:key="todo.id">
-            <td>{{todo.todo_name}}</td>
-            <td>{{todo.todo_time}}</td>
-            <td><input type="checkbox"/></td>
-            <td><button>Delete</button></td>
-        </tr>
-    </table>
-    <br><br>
-
-   <h>AddToDo</h>
-    <!-- <form v-on:submit="addToDo">
-      <input type="text" v-model="post.name" placeholder="Product Name" />
-      <input type="number" v-model="post.price" placeholder="Price" />
-      <button type="submit">Add</button>
-    </form> -->
-
-    <form @submit="addToDo">
-      <input type="text" placeholder="todo_name" />
-      <input type="text" placeholder="todo_time" />
-      <button type="submit">Add</button>
-    </form> 
-  </div>
+    <div align="center">
+        <button @click="getSelectedRows()">Get Selected Rows</button>
+        <br><br>
+        <button @click="console.log(rowData)">
+            show in console
+        </button>
+        <br><br>
+         <button @click="buttonClick">X</button>
+         <br><br>
+         <button @click="addrow">AddRow</button>
+        <ag-grid-vue style="width: 500px; height: 500px;"
+            class="ag-theme-alpine"
+            :columnDefs="columnDefs"
+            :rowData="rowData"
+            rowSelection="multiple"
+            @grid-ready="onGridReady">
+        </ag-grid-vue>
+    </div>
 </template>
 
 
 
 <script>
+import "ag-grid-community/dist/styles/ag-grid.css";
+// import "ag-grid-community/dist/styles/ag-theme-alpine.css";
+import { AgGridVue } from "ag-grid-vue";
 export default {
   name: "ToDo",
 
-  data: function () {
+  data () {
     return {
-      todolist: [
-                 { todo_name:"bathing" , todo_time:"7:30"},
-                 { todo_name:"yoga" , todo_time:"8:30"},
-                 { todo_name:"rest" , todo_time:"9:00"},
-                 { todo_name:"breakfast" , todo_time:"9:15"},
-                 { todo_name:"study" , todo_time:"10:00"}
-                ]
-    };
-  },
-  methods: {
-    addToDo(e) {
-    
-      e.preventDefault(); // it prevent from page reload
+         columnDefs: null,
+         rowData: null,
+         gridApi: null,
+         columnApi: null
     }
-  }
-}
+  },
+   components: {
+            AgGridVue
+        },
+
+   methods: {
+            onGridReady(params) {
+                this.gridApi = params.api;
+                this.columnApi = params.columnApi;
+            },
+            addrow(){
+                this.gridApi.applyTransaction({add:[{}]})
+
+            },
+            getSelectedRows() {
+                const selectedNodes = this.gridApi.getSelectedNodes();
+                const selectedData = selectedNodes.map( node => node.data );
+                const selectedDataStringPresentation = selectedData.map( node => `${node.make} ${node.model}`).join(', ');
+                alert(`Selected nodes: ${selectedDataStringPresentation}`);
+            },
+            defaultColDef: {
+    suppressKeyboardEvent: params => {
+        if (!params.editing) {
+            let isBackspaceKey = params.event.keyCode === 8;
+            let isDeleteKey = params.event.keyCode === 46;
+			
+            if (isBackspaceKey) {
+                // Delete all selected rows...
+				return true
+			}
+			
+			if(isDeleteKey){
+                // Delete all selected cell ranges...
+                return true
+			}
+        }
+        return false;
+    }
+ },
+buttonClick (e) {
+
+        this.setState({
+            visible:true
+        })
+        let deletedRow = this.props.node.data;
+        e.gridApi.updateRowData({ remove: [deletedRow] })  // It will update the row
+    }
+    
+   },
+            
+
+        
+   beforeMount() {
+            this.columnDefs = [
+                { field: 'todo_name', editable:true, sortable: true, filter: true, checkboxSelection: true},
+                { field: 'todo_time' , editable:true, sortable: true, filter: true,checkboxSelection: true}
+            ];
+
+            this.rowData = [
+                {todo_name:'bathing',todo_time:'7:30'},
+                {todo_name:'yoga',todo_time:'8:30'},
+                {todo_name:'rest',todo_time:'9:00'},
+                {todo_name:'breakfast',todo_time:'9:15'},
+                {todo_name:'study',todo_time:'10:00'}
+                
+            ];
+        }
+    }    
+  
+
 </script>
+
